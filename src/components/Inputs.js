c201975615d3a8c4a6e145c9d141fe2060ea502b
Artem Shkuratov <shuratov2001@mail.ru>
2021-02-16 06:01:41 +0800

    added style for inputs component

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -3,15 +3,32 @@ import React from 'react';
 
 function Inputs(){
     return(
-        <div>
-            <p>lorem5 sddfsf
-                ddsfdsf
-                ddsfdsff
-                sddfsf
-                fsd
-                ddsfdsfsdf
-            </p>
-        </div>
+        <form className="form-inline">
+            <div className="form-group mx-sm-3 mb-2">
+                <label for="inputPassword2" class="sr-only">Task Name</label>
+                <input type="text" class="form-control" id="inputPassword2" placeholder="task name"></input>
+            </div>
+            <div className="form-group mx-sm-3 mb-2">
+                <label for="inputPassword2" class="sr-only">status</label>
+                <input type="text" class="form-control" id="inputPassword2" placeholder="status" readonly></input>
+            </div>
+            <div className="form-group mx-sm-3 mb-2">
+            <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                <option selected>Choose...</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+            <div className="form-group mx-sm-3 mb-2">
+                <input type="date" class="form-control" placeholder="Date"></input>
+            </div>
+            </div>
+            <div className="form-group mx-sm-3 mb-2">
+                {/* <label for="exampleFormControlFile1">Example file input</label> */}
+                <input type="file" class="form-control-file" id="exampleFormControlFile1"></input>
+            </div>
+            <button type="submit" class="btn btn-primary mb-2">Add new task</button>
+        </form>
     )
 }
 
