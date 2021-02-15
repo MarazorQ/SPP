067167f33f72cbbbd27b2369e8954c1dc7cc8c28
Artem Shkuratov <shuratov2001@mail.ru>
2021-02-16 04:35:51 +0800

    added folder for components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
+import App from './components/App';
 
-function HelloWorld() {
-    return(
-        <div>
-            <h1>hellow wordlhhhh</h1>
-        </div>
-    )
-    
-}
 
-render(<HelloWorld/>, document.getElementById('root'));
+render(<App/>, document.getElementById('root'));
