cb6169b041747bcbe33b37205d6cdfd8a534aab3
Artem Shkuratov <shuratov2001@mail.ru>
2021-02-16 06:28:22 +0800

    added content to table for example

--- src/components/Frame.js
+++ src/components/Frame.js
@@ -9,6 +9,29 @@ function Frame(){
                 <th>Date of Completion</th>
                 <th>File</th>
             </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td><button class="btn btn-primary">Download</button></td>
+                </tr>
+                <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                <td><button class="btn btn-primary">Download</button></td>
+                </tr>
+                <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+                <td><button class="btn btn-primary">Download</button></td>
+                </tr>
+            </tbody>
         </table>
     )
 }
