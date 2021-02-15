150853750e02ddc5bce3d13ccc594de7a7f0c6e5
Artem Shkuratov <shuratov2001@mail.ru>
2021-02-16 04:50:46 +0800

    added new component for application

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Inputs from './Inputs';
 
 function App() {
     return(
         <div>
             <h1>Task Manager</h1>
+            <Inputs/>
         </div>
     )
     
---
+++ src/components/Inputs.js
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+function Inputs(){
+    return(
+        <div>
+            <p>lorem5 sddfsf
+                ddsfdsf
+                ddsfdsff
+                sddfsf
+                fsd
+                ddsfdsfsdf
+            </p>
+        </div>
+    )
+}
+
+export default Inputs
