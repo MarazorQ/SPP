07a50ff0a90d9c5dfe5139ffb749a074e0906836
Artem Shkuratov <shuratov2001@mail.ru>
2021-02-17 20:23:26 +0800

    get value from input

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -3,29 +3,33 @@ import React from 'react';
 
 
 class Inputs extends React.Component{
-    state = {
-        name: 'Бумеранг вернулся назад'
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+    
+    handleChange(event){
+        this.setState({value:event.target.value});
+    }
 
     render(){
         return(
-            <form className="form-inline" onSubmit={()=> {this.props.updateData(this.state.name)}}>
+            <form className="form-inline" onSubmit={()=> {this.props.updateData(this.state.value)}}>
                 <div className="form-group mx-sm-3 mb-2">
                     <label for="inputPassword2" class="sr-only">Task Name</label>
-                    <input type="text" className="form-control" id="inputPassword2" placeholder="Task name"></input>
+                    <input type="text" className="form-control" id="inputPassword2" placeholder="Task name" value={this.state.value} onChange={this.handleChange}></input>
                 </div>
-                {/* <div className="form-group mx-sm-3 mb-2">
-                    <label for="inputPassword2" class="sr-only">status</label>
-                    <input type="text" className="form-control" id="inputPassword2" placeholder="status" readonly></input>
-                </div> */}
                 <div className="form-group mx-sm-3 mb-2">
-                <select className="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                    <option selected>Status</option>
-                    <option value="1">To Do</option>
-                    <option value="2">Doing</option>
-                    <option value="3">Done</option>
-                    <option value="4">Questions</option>
-                </select>
+                    <select className="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                        <option selected>Status</option>
+                        <option value="1">To Do</option>
+                        <option value="2">Doing</option>
+                        <option value="3">Done</option>
+                        <option value="4">Questions</option>
+                    </select>
                 <div className="form-group mx-sm-3 mb">
                     <input type="date" className="form-control" placeholder="Date"></input>
                 </div>
@@ -38,7 +42,6 @@ class Inputs extends React.Component{
             </form>
         )
     }
-
 }
 
 export default Inputs
